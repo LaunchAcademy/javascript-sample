8443f6082eab555f723934f8a2de9a0c8c69f641
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-25 11:34:25 -0500

    Add client-side validations to new article form

--- public/js/articles.js
+++ public/js/articles.js
@@ -4,3 +4,28 @@ $("#show-form").on("click", function(event) {
   $("#new-article-form").show();
   $("#show-form").hide();
 });
+
+$("#new-article-form").on("submit", function(event) {
+  var title = $("#title").val();
+  var url = $("#url").val();
+  var foundError = false;
+
+  if (title.length === 0) {
+    $("#new-article-form").append("<p class='errors'>Title can't be blank.</p>");
+    foundError = true;
+  }
+
+  if (url.length === 0) {
+    $("#new-article-form").append("<p class='errors'>URL can't be blank.</p>");
+    foundError = true;
+  } else if (url.indexOf("http") !== 0) {
+    $("#new-article-form").append("<p class='errors'>URL must start with http.</p>");
+    foundError = true;
+  }
+
+  if (foundError) {
+    return false;
+  } else {
+    return true;
+  }
+});
