8d8ac37f08a087f0675482acc838694a92d6cb81
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-24 19:18:56 -0500

    Display list of articles

---
+++ server.rb
@@ -0,0 +1,47 @@
+require "sinatra"
+require "csv"
+
+get "/articles" do
+  erb :index, locals: { articles: list_articles, errors: [] }
+end
+
+post "/articles" do
+  errors = input_errors(params[:title], params[:url])
+
+  if errors.empty?
+    puts "no errors"
+    add_article(params[:title], params[:url])
+    redirect "/articles"
+  else
+    puts "errors"
+    erb :index, locals: { articles: list_articles, errors: errors }
+  end
+end
+
+def list_articles
+  CSV.read("articles.csv").map do |row|
+    { title: row[0], url: row[1] }
+  end
+end
+
+def input_errors(title, url)
+  errors = []
+
+  if title.nil? || title.empty?
+    errors << "Title cannot be blank"
+  end
+
+  if url.nil? || url.empty?
+    errors << "URL cannot be blank"
+  elsif !url.start_with?("http")
+    errors << "URL must start with http(s)"
+  end
+
+  errors
+end
+
+def add_article(title, url)
+  CSV.open("articles.csv", "a") do |csv|
+    csv << [title, url]
+  end
+end
